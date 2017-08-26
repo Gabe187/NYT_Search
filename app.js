043be7aa79065fa530fca68fe951b5454d38df29
Laura Boetcher <g33klaura@gmail.com>
2017-08-26 12:10:23 -0400

    Update queryURL and some search terms

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // NYT API Key 7fd1e0cad1704cbaa03053038356dc73
 
 /*
-* Create the AJAX call needed to retrieve data then console.log all of the relevant fields.
 
 * Incorporate various “optional parameters” (hard code these in initially).
 
 * Take note of various “bugs” that appear with certain searches.
-
-## All-Together
-
-* Display the HTML content!!
 */
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=7fd1e0cad1704cbaa03053038356dc73";
+// Variable to hold NYT Article Search Page
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-// From NYT 
-// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-// url += '?' + $.param({
-//   'api-key': "7fd1e0cad1704cbaa03053038356dc73"
-// });
+// Adds parameters of API key and search terms
+queryURL += '?' + $.param({
+  'api-key': "7fd1e0cad1704cbaa03053038356dc73",
+  // q = 'search query term'
+  'q': "pablo picasso",
+  // dates must be YYYYMMDD
+  'begin_date': "19700101",
+  'end_date': "19850101"
+});
 
 $.ajax({
 	url: queryURL,
